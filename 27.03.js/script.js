ad5685e6aca25b98ae3e399d418f4b35acd1cdc4
ayanabdullay3va <bd867CKFS@code.edu.az>
2024-03-27 20:00:07 +0400

    js intro

---
+++ 27.03.js/script.js
@@ -0,0 +1,27 @@
+// let value = 200
+// let minutes = parseInt(value / 60)
+// let second = (value - minutes * 60)
+// console.log(minutes, second)
+
+// let a, b, c, d, e;
+// a = +window.prompt("enter your first num");
+// b = +window.prompt("enter your second num");
+// c = +window.prompt("enter your third num");
+// d = +window.prompt("enter your fourth num");
+// e = +window.prompt("enter your fifth num");
+// let result = (a + b + c + d + e) / 5;
+// console.log(result);
+
+
+// let il = 2024
+// let birth = +window.prompt("enter your birthyear")
+// let yas = (il - birth)
+// console.log(yas);
+
+// let a, b
+// a = +window.prompt("enter your first num");
+// b = +window.prompt("enter your second num");
+// let faiz = (a / b * 100)
+// console.log(faiz)
+
+let kat1 =
