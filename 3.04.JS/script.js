2f8dc34636e82f264aa8f43e6ee328c28a370116
ayanabdullay3va <bd867CKFS@code.edu.az>
2024-04-03 19:22:28 +0400

    03.04.js-method

---
+++ 3.04.JS/script.js
@@ -0,0 +1,118 @@
+let arr = [
+    {
+        name: "test",
+        key: 1,
+    },
+    {
+        name: "task",
+        key: 2,
+    },
+    {
+        name: "tanqo",
+        key: 3,
+    },
+    {
+        name: "like",
+        key: 4,
+    },
+    {
+        name: "task",
+        key: 5,
+    },
+    {
+        name: "trust",
+        key: 6,
+    },
+    {
+        name: "test",
+        key: 7,
+    },
+    {
+        name: "last",
+        key: 8,
+    },
+    {
+        name: "tanqo",
+        key: 9,
+    },
+    {
+        name: "elephant",
+        key: 10,
+    },
+    {
+        name: "love",
+        key: 11,
+    },
+    {
+        name: "small",
+        key: 12,
+    },
+    {
+        name: "little",
+        key: 13,
+    },
+];
+//task-1
+// const result = arr.filter((item) => {
+//     return item.name.toLocaleLowerCase().startsWith("t")
+
+// })
+// console.log(result)
+
+//task-2
+// let count = 0
+// const result = arr.forEach((item) => {
+//     if (item.name.toLocaleLowerCase().startsWith("t") && item.name.toLocaleLowerCase().endswith("t") ) {
+
+//         count++;
+//     }
+
+// })
+// console.log(count)
+
+// task-3
+// let sum = 0
+// const result = arr.forEach((item) => {
+//     if (item.name.toLocaleLowerCase().startsWith("t") && item.name.toLocaleLowerCase().endsWith("t")) {
+//         sum += item.key
+
+//     }
+// })
+// console.log(sum)
+
+//task-4
+// const result = arr.forEach((item) => {
+//     if (item.name.toLocaleLowerCase().endsWith("e")) {
+//         item.name = "SuperDev"
+//     }
+// })
+// console.log(arr)
+
+// task-5
+// let max = arr[0]
+// arr.forEach((item) => {
+//     if (item.name.length > max.name.length) {
+//         max = item
+//     }
+// })
+// console.log(max)
+
+//task-6
+// let max = arr[0]
+// arr.forEach((item) => {
+//     if (item.name.length > max.name.length) {
+//         max = item
+//     }
+// })
+// console.log(max.key)
+
+// task-7
+// let max = arr[0]
+// let maxIndex = 0
+// arr.forEach((item) => {
+//     if (item.name.length > max.name.length) {
+//         max = item
+//         maxIndex = index
+//     }
+// })
+// console.log(maxIndex ** 2)
