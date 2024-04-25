d608dcfcb34f95b503088cfbf4899d54307c1c9b
Sefermuradov <bd5fq7ynp@code.edu.az>
2024-04-25 19:47:06 +0400

    react-promp

---
+++ 25.04.REACT/vite-project/src/components/AddForm/index.jsx
@@ -0,0 +1,14 @@
+const AddForm = () => {
+  return (
+    <form>
+      <label htmlFor="">Course Name</label> <br />
+      <input type="text" /> <br />
+      <label htmlFor="">Course Price</label> <br />
+      <input type="number " /> <br />
+      <label htmlFor="">Course Image</label> <br />
+      <input type="text" />
+    </form>
+  );
+};
+
+export default AddForm;
