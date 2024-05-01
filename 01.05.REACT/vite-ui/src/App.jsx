5afd530bec06a1f3302ad47d4776e3248a355803
Sefermuradov <bd5fq7ynp@code.edu.az>
2024-05-01 19:54:34 +0400

    mui-task

---
+++ 01.05.REACT/vite-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import PrimarySearchAppBar from "./layouts/Header";
+import "./App.css";
+import Cards from "./components/Cards";
+// import ImgMediaCard from "./components/Card";
+function App() {
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <Cards />
+    </>
+  );
+}
+
+export default App;
