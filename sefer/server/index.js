1e01c9b5336cf7f08d78975f1a8e33f8bc1d6701
lemanshamilova <[email>
2024-05-17 14:22:48 +0400

    test1

---
+++ sefer/server/index.js
@@ -0,0 +1,125 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const { Schema } = mongoose;
+
+const DB = process.env.DB_URL
+const PORT = process.env.PORT
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+mongoose.connect(DB).then(() => {
+    console.log("connected");
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`)
+    })
+},
+    (err) => {
+        console.log(err);
+    }
+);
+
+const menuSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    ingredient: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+
+});
+
+const Menus = mongoose.model("menus", menuSchema);
+
+app.get('/menus', async (req, res) => {
+    try {
+        const menus = await Menus.find({})
+        console.log(menus);
+        if (menus.length > 0) {
+            res.status(200).send({
+                message: "succes",
+                error: null,
+                data: menus
+            })
+        } else {
+            res.send({
+                message: "data is empty",
+                data: null
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
+})
+
+app.get('/menus/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const menu = await Menus.findById(id)
+        if (menu != undefined) {
+            res.status(200).send({
+                message: "succes",
+                error: null,
+                data: menu
+            })
+        } else {
+            res.send({
+                message: "data is empty",
+                data: null
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
+})
+
+app.delete('/menus/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedMenu = await Menus.findByIdAndDelete(id)
+        const menus = await Menus.find({})
+        res.status(200).send({
+            message: "deleted",
+            deletedMenu: deletedMenu,
+            allMenus: menus
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+
+    }
+})
+
+app.post("/menus", async (req, res) => {
+    const newmenu = new Menus({ ...req.body });
+    try {
+        await newmenu.save();
+        res.status(201).send({
+            message: "posted successfully",
+            data: newmenu,
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message,
+        });
+    }
+});
